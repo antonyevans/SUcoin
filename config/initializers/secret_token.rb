50ea73b9c329882fb52572a32c91c3372a6ca7cf
Antony Evans <antonyevans@gmail.com>
2011-07-01 14:14:01 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sucoin::Application.config.secret_token = '4caef1b96afabde3c4e30123efb0687cdd8f6d16ab2aaf83dba141f576ec740840029a6cf83cfdc011ddf882da85c4f5711fa70e731587a12e8b355ff0c5b9e3'
